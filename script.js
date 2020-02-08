991ace771c302859081c11b3174ba8417c502469
TharunKaushal <tharunkaushal@gmail.com>
2020-02-08 19:45:45 +0530

    Fixed failed/absent results

--- script.js
+++ script.js
@@ -26,8 +26,12 @@ function calculateGrade() {
     let sub7 = document.getElementById("sub7")
     let sub8 = document.getElementById("sub8")
     let sub9 = document.getElementById("sub9")
-
-    let cum_sum = (sub1.value*2 + sub2.value*2 + sub3.value*2 + sub4.value*1 + sub5.value*4 + sub6.value*4 + sub7.value*3 + sub8.value*3 + sub9.value*3)
+    let cum_sum
+    if (sub1.value == 0 || sub2.value == 0 || sub3.value == 0 || sub4.value == 0 || sub5.value == 0 || sub6.value == 0 || sub7.value == 0 || sub8.value == 0 || sub9.value == 0) {
+        cum_sum = 0
+    } else {
+        cum_sum = (sub1.value*2 + sub2.value*2 + sub3.value*2 + sub4.value*1 + sub5.value*4 + sub6.value*4 + sub7.value*3 + sub8.value*3 + sub9.value*3)
+    }
     let credits = 24
     let result_grade = cum_sum / credits
     document.getElementById("result").innerHTML = result_grade.toFixed(2)
